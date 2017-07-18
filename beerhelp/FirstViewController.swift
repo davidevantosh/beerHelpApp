f8b359a6cb0253d863ece017a9a9975f5c858093
David Tosh <davidtosh@davids-imac.fritz.box>
2017-07-19 08:42:56 +1200

    Initial Commit

---
+++ beerhelp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  beerhelp
+//
+//  Created by David Tosh on 19/07/17.
+//  Copyright © 2017 David Tosh. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
